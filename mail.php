7b8414ac648b59d91a32a787ad049dcdd81f30a5
drKopp <117771646+drKopp@users.noreply.github.com>
2024-12-14 13:22:39 +0100

    mail.php

--- mail.php
+++ mail.php
@@ -19,7 +19,7 @@
 
         // Set the recipient email address.
         // FIXME: Update this to your desired email address.
-        $recipient = "random@example.com";
+        $recipient = "dirianatins@example.com";
 
         // Set the email subject.
         $subject = "$subject";
